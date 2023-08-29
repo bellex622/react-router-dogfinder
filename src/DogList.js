82416b270e7e04cecef6fe2e321b125e771e0b02
Belle Xu <bellex22@protonmail.com>
2023-08-28 21:02:39 -0400

    fix img issue

--- src/DogList.js
+++ src/DogList.js
@@ -7,7 +7,7 @@ function DogList({ dogList }) {
   return (
     <ul>{dogList.map(dog =>
       <li key={dog.name}>name:{dog.name} age:{dog.age}
-        <img src="../public/{dog.src}" />
+        <img src={`/${dog.src}.jpg`} />
       </li>)}</ul>
   );
 
